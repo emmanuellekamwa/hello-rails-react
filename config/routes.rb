3cd0e067ad88e1fe2ebf29d32ab472fdab8eb80a
emmanuellekamwa <emmanuellekamwa@gmail.com>
2022-02-02 11:06:33 +0100

    Refactor greetings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
-Rails.application.routes.draw do
-  get '*page', to: 'static#index', constraints: -> (req) do 
-    !req.xhr? && req.format.html?
-  end
-  
-  namespace :api do 
+Rails.application.routes.draw do 
     resources :greetings, only: [:index, :show] do
     end
-  end
 
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
