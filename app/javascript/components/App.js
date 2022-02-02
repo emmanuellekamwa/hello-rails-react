92633ba8204fa9d06c24958e6ec9fcf2632013cb
emmanuellekamwa <emmanuellekamwa@gmail.com>
2022-02-02 13:23:09 +0100

    Generate react component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Greeting from './Greeting'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './Greeting';
+import store from '../redux/configureStore';
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Greeting />} />
+          <Route path="/" element={<Greeeting />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,18 +1,16 @@
 import React from "react"
-import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from "prop-types"
+class Greeting extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Title: {this.props.title}
+      </React.Fragment>
+    );
+  }
+}
 
-store.dispatch(getGreetingData);
-const Greeting = () => {
-  const greetings = useSelector((state) => state.greetingData);
-  const greetingArr = [];
-  const randomNumber = Math.floor(Math.random() * 5);
-
-  Object.entries(greetings).map(([key, value]) =>
-    greetingArr.push(value['greetings'])
-  );
-
-  return <p className='greeting'>{greetingArr[randomNumber]}</p>;
+Greeting.propTypes = {
+  title: PropTypes.string
 };
-
-
-export default Greeting;
+export default Greeting
